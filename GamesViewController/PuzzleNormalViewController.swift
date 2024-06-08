54ebe84d9e2fc95e2bc84b026377e0d46bcce374
Nikunj <tyaginikunj26@gmail.com>
2024-06-08 11:08:27 +0530

    added a puzzle view controller

---
+++ GamesViewController/PuzzleNormalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PuzzleNormalViewController.swift
+//  DemoAlzyCare
+//
+//  Created by Nikunj Tyagi on 08/06/24.
+//
+
+import UIKit
+
+class PuzzleNormalViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
